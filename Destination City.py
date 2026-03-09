0c44c312db98bff99a2cc6f91ea4f2c261f16ddd
duchoai039-ctrl <duchoai039@gmail.com>
2026-03-09 18:32:25 +0700

    Add files via upload

---
+++ Destination City.py
@@ -0,0 +1,10 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        start = set()
+
+        for a, b in paths:
+            start.add(a)
+
+        for a, b in paths:
+            if b not in start:
+                return b
