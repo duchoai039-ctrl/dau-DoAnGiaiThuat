262511e439157c115babd58318a7ee8e71a8974e
duchoai039-ctrl <duchoai039@gmail.com>
2026-05-12 16:08:42 +0700

    Add files via upload

---
+++ Move Zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums):
+        j = 0 # Pointer to place the next non-zero element
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                # Swap current element with the element at index j 
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1 # Move j to the next index for placing non-zero
+
+        
+        
